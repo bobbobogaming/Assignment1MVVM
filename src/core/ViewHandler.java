847712fd72bb4b2cd725e0e67e3b7b4a95251fa5
Anders hellesøe <anders@hellesoee.dk>
2022-03-11 20:51:52 +0100

    clean up of ViewHandler

--- src/core/ViewHandler.java
+++ src/core/ViewHandler.java
@@ -5,7 +5,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import view.main.TabViewController;
-import view.radiator.RadiatorViewModel;
 
 import java.io.IOException;
 
@@ -14,7 +13,6 @@ public class ViewHandler
   private Scene currentScene;
   private Stage currentStage;
   private ViewModelFactory viewModelFactory;
-  private RadiatorViewModel radiatorViewModel;
 
   public ViewHandler(Stage stage, ViewModelFactory viewModelFactory)
   {
@@ -30,7 +28,7 @@ public class ViewHandler
     Parent root = loader.load();
     TabViewController tabViewController = loader.getController();
     tabViewController.init(viewModelFactory.getTemperatureViewModel(),viewModelFactory.getRadiatorViewModel());
-    currentStage.setTitle("Tab view");
+    currentStage.setTitle("Thermostat system");
 
     currentScene = new Scene(root);
     currentStage.setScene(currentScene);
